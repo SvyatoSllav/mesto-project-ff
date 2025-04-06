1c3ac2f6d5fe026c0cf822b09e4324fab6fe99cf
polarius <tarasov.slavas2002@gmail.com>
2025-04-06 14:21:37 +0300

    Фикс подгрузки картинок, стилей и main.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,74 @@
-// webpack.config.js
-const path = require('path'); // подключаем path к конфигу вебпак
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
-
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
-        publicPath: '/'
+        publicPath: '', // Keep this as empty string for relative paths
     },
     mode: 'development',
     devServer: {
-        static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-        compress: true, // это ускорит загрузку в режиме разработки
-        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-
-        open: true // сайт будет открываться сам при запуске npm run dev
+        static: path.resolve(__dirname, './dist'),
+        compress: true,
+        port: 8080,
+        open: true
     },
     module: {
-        rules: [ // rules — это массив правил
-            // добавим в него объект правил для бабеля
+        rules: [
             {
-                // регулярное выражение, которое ищет все js файлы
                 test: /\.js$/,
-                // при обработке этих файлов нужно использовать babel-loader
                 use: 'babel-loader',
-                // исключает папку node_modules, файлы в ней обрабатывать не нужно
                 exclude: '/node_modules/'
             },
             {
-                // регулярное выражение, которое ищет все файлы с такими расширениями
-                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-                type: 'asset/resource'
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]' // This will keep original filenames
+                }
             },
             {
-                // применять это правило только к CSS-файлам
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                }
+            },
+            {
                 test: /\.css$/,
-                // при обработке этих файлов нужно использовать
-                // MiniCssExtractPlugin.loader и css-loader
-                use: [MiniCssExtractPlugin.loader, {
-                    loader: 'css-loader',
-                    options: { importLoaders: 1 }
-                },
-                'postcss-loader'
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { importLoaders: 1 }
+                    },
+                    'postcss-loader'
                 ]
             },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                }
+            }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html' // путь к файлу index.html
+            template: './src/index.html'
         }),
         new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-        new CopyPlugin({
-            patterns: [{
-                from: path.resolve(__dirname, 'src/images'),
-                to: path.resolve(__dirname, 'dist/images')
-            }]
-        }
-        ),
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+        }),
     ]
 }
-
-// переписали точку выхода, используя утилиту path
